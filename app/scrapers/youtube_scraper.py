50422715f1640dc5baf7787ec5ba4f2fd5ed2c77
RahulArora0603 <rahul.arora0603@gmail.com>
2025-11-05 21:03:50 +0530

    yt exception

--- app/scrapers/youtube_scraper.py
+++ app/scrapers/youtube_scraper.py
@@ -4,7 +4,6 @@ from youtubesearchpython import VideosSearch
 # Function to get YouTube videos based on a query (default "artificial intelligence")
 def get_youtube_videos(query = "artificial intelligence"):
     try:
-
         videos_search = VideosSearch(query, limit=10)
         results = videos_search.result()["result"]
 
@@ -18,4 +17,4 @@ def get_youtube_videos(query = "artificial intelligence"):
         return video_list # return a list of dictionaries containing title, url, and image for each video
     except Exception as e:
         #print(f"An error occurred while fetching YouTube videos: {e}")
-        return [{"title": "Rate limit reached or temporary error", "url": "#", "image": None}]  # return an empty list in case of error
+        return [{"status": "error", "message": str(e)}]  # return an empty list in case of error
